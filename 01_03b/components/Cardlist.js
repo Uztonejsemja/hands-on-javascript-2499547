76915e27340ab31f9880d6bfb9b2227b6550c353
Jolana Unverdorben <98396890+Uztonejsemja@users.noreply.github.com>
2026-07-02 07:49:04 +0000

    cardlistitem

--- 01_03b/components/Cardlist.js
+++ 01_03b/components/Cardlist.js
@@ -1,5 +1,19 @@
 import Card from "./Card.js";
 
-const Cardlist = (data) => {};
+const cardlistItem = (imgData) => {
+  return `
+  <li class="cardlist_item">
+  ${Card(imgData)}
+  </li>`
+};
+
+const Cardlist = (data) => {
+  return `
+  <section class="cardlist">
+    <ul class="cardlist_list">
+    ${data.map((imgData) => cardlistItem(imgData)).join("")}
+    </ul>
+  </section>`
+};
 
 export default Cardlist;
